883ea67e920bc1dd5a8c9ff14d6c8a8318fd8397
makometr <makometr@yandex.ru>
2021-07-23 01:42:52 +0300

    Task5 complete

--- Read_Understand/task5/task5.go
+++ Read_Understand/task5/task5.go
@@ -1,1 +1,31 @@
-package task5
+package main
+
+import "fmt"
+
+type customError struct {
+	msg string
+}
+
+func (e *customError) Error() string {
+	return e.msg
+}
+
+func test() *customError {
+	{
+		// do something
+	}
+	return nil
+}
+
+func main() {
+	var err error
+	err = test()
+	if err != nil {
+		println("error") // напишет это, т.к. тип хранящего внутри обхекта не nil, a *customError
+		fmt.Printf("%T", err)
+		return
+	}
+	println("ok") // а не это
+}
+
+// Для исправления test должен возвращать объекты типа интерфейс error
