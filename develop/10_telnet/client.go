d83f6a367385b56f95d3d612dc450fdf10611c08
makometr <makometr@yandex.ru>
2021-08-05 02:14:48 +0300

    Urra task 10 done!

---
+++ develop/10_telnet/client.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"time"
+)
+
+type TelnetClient struct {
+	address    string
+	timeout    time.Duration
+	conn       net.Conn
+	connReader *bufio.Reader
+}
+
+func NewTelnetClient(address string, timeout time.Duration) *TelnetClient {
+	res := &TelnetClient{
+		address: address,
+		timeout: timeout,
+	}
+	return res
+}
+
+func (t *TelnetClient) initConnection() error {
+	conn, err := net.DialTimeout("tcp", t.address, t.timeout)
+	if err != nil {
+		return fmt.Errorf("%s : %w", "Conncetion error", err)
+	}
+	t.conn = conn
+	t.connReader = bufio.NewReader(t.conn)
+	fmt.Printf("Connected success to  %v\n", t.address)
+	return nil
+}
+
+func (t *TelnetClient) closeConnection() error {
+	return t.conn.Close()
+}
+
+func (t *TelnetClient) receieveMsg() error {
+	line, err := t.connReader.ReadString('\n')
+	if err != nil {
+		if err == io.EOF {
+			err = fmt.Errorf("error: closed by peer")
+		}
+		return err
+	}
+	if _, err := fmt.Print(line); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *TelnetClient) sendMsg(msg string) error {
+	_, err := t.conn.Write([]byte(msg))
+	if err != nil {
+		return fmt.Errorf("error: closed by peer")
+	}
+	return nil
+}
--- develop/10_telnet/main.go
+++ develop/10_telnet/main.go
@@ -2,51 +2,85 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
-	"net"
+	"log"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
-type TelnetClient struct {
-	address    string
-	port       int
-	timeout    time.Duration
-	conn       net.Conn
-	connReader *bufio.Reader
-}
+func getDataFromUser() (string, time.Duration) {
+	timeout := flag.Duration("timeout", 10*time.Second, "timeout in seconds")
+	flag.Parse()
+	args := flag.Args()
 
-func NewTelnetClient(address string, port int, timeout time.Duration) *TelnetClient {
-	res := &TelnetClient{
-		address: address,
-		port:    port,
-		timeout: timeout,
+	if len(args) != 2 {
+		log.Fatalf("usage: go-telnet --timeout=10s host port")
 	}
+	host := args[0]
+	port := args[1]
 
-	return res
-}
-
-func (tc *TelnetClient) Connect() error {
-	conn, err := net.DialTimeout("tcp", tc.address, tc.timeout)
-	if err != nil {
-		return fmt.Errorf("%s : %w", "error dial to host", err)
-	}
-	tc.conn = conn
-	tc.connReader = bufio.NewReader(tc.conn)
-	fmt.Fprintf(os.Stderr, "%s %s\n", "err: closed by peer", tc.address)
-	return nil
-}
-
-func (tc *TelnetClient) Close() error {
-	return tc.conn.Close()
+	return host + ":" + port, *timeout
 }
 
 func main() {
-	client := NewTelnetClient("/", 8080, time.Second*3)
-	err := client.Connect()
-	if err != nil {
-		fmt.Println(err)
-		return
+	client := NewTelnetClient(getDataFromUser())
+	if err := client.initConnection(); err != nil {
+		log.Fatalf("error while connecting %v", err)
 	}
-	client.Close()
+	defer func() {
+		client.closeConnection()
+	}()
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	errors := make(chan error, 1)
+
+	go func() {
+		for {
+			err := client.receieveMsg()
+			if err != nil {
+				errors <- err
+				return
+			}
+		}
+	}()
+
+	go func() {
+		// Чтобы ловить EOF
+		inputReader := bufio.NewReader(os.Stdin)
+		for {
+			line, err := inputReader.ReadString('\n')
+			if err != nil {
+				errors <- err
+				return
+			}
+			if err := client.sendMsg(line); err != nil {
+				errors <- err
+				return
+			}
+		}
+	}()
+
+	closed := make(chan interface{}, 0)
+	go func() {
+		defer close(closed)
+		for {
+			select {
+			case <-signals:
+				return
+			case err := <-errors:
+				fmt.Println(err)
+				if err != nil {
+					return
+				}
+			default:
+				continue
+			}
+		}
+	}()
+
+	<-closed
 }
