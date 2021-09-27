ffef80cf0fd5bc0ed4412d20ee94fb6aae7cdfc5
makometr <makometr@yandex.ru>
2021-09-27 03:05:50 +0300

    Update filenames

--- develop/dev01_clock/main.go
+++ develop/dev01_clock/main.go
@@ -2,17 +2,18 @@ package basedevelop
 
 import (
 	"fmt"
+	"go-tasks-2/develop/dev01_clock/wbtimer/clock"
 	"log"
-
-	"github.com/beevik/ntp"
 )
 
 // PrintCurrentTime prints curremt time from remote server
 func PrintCurrentTime() {
-	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	// time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	c, err := clock.New(clock.BaseHost)
+
 	if err != nil {
 		log.Fatalln("Error!", err)
 	}
 
-	fmt.Println("Current time:", time)
+	fmt.Println("Current time:", c.time)
 }
---
+++ develop/dev01_clock/wbtimer/go.mod
@@ -0,0 +1,8 @@
+module go-tasks-2/develop/dev01_clock/wbtimer
+
+go 1.16
+
+require (
+	github.com/beevik/ntp v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20210924151903-3ad01bbaa167 // indirect
+)
